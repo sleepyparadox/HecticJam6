97ba575be26895ef1a7ca2842e7e66fc6daf93f4
Peter Liang <peterdhl88@gmail.com>
2015-06-07 16:29:42 +1000

    Bullet & Enemy Changes

--- ProjectVirtualWorld/Unity/Assets/Code/BadStuff/Bullet.cs
+++ ProjectVirtualWorld/Unity/Assets/Code/BadStuff/Bullet.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public enum MoveModifier {Straight, Curve, Sine, Loop, ZigZag}
@@ -17,33 +18,7 @@ public class Bullet
 	public Vector2 angularVel;
 	public float angle;
 
-	IEnumerator Modify ()
-	{
-		float count = 0;
-		while (true)
-		{
-			if (modifier == MoveModifier.Straight)
-			{
-				var direction = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
-				angularVel = direction * speed;
-			}
-			else if (modifier == MoveModifier.Curve)
-			{
-				count += Time.deltaTime * modifierIntensity;
-				var curveAngle = angle + count;
-				var direction = new Vector2(Mathf.Sin(curveAngle), Mathf.Cos(curveAngle));
-				angularVel = direction * speed;
-			}
-			else if (modifier == MoveModifier.Sine)
-			{
-				count += Time.deltaTime * modifierIntensity;
-				var sineAngle = angle + Mathf.Sin(count);
-				var direction = new Vector2(Mathf.Sin(sineAngle), Mathf.Cos(sineAngle));
-				angularVel = direction * speed;
-			}
-			yield return null;
-		}
-	}
+    private float count = 0;
 	
 	// Update is called once per frame
     public void Update()
@@ -51,6 +26,40 @@ public class Bullet
 		_angularPos += angularVel * Time.deltaTime;
 		
 		lifespan -= Time.deltaTime;
+
+        if (modifier == MoveModifier.Straight)
+        {
+            var direction = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+            angularVel = direction * speed;
+        }
+        else if (modifier == MoveModifier.Curve)
+        {
+            count += Time.deltaTime * modifierIntensity;
+            var curveAngle = angle + count;
+            var direction = new Vector2(Mathf.Sin(curveAngle), Mathf.Cos(curveAngle));
+            angularVel = direction * speed;
+        }
+        else if (modifier == MoveModifier.Sine)
+        {
+            count += Time.deltaTime * modifierIntensity;
+            var sineAngle = angle + Mathf.Sin(count);
+            var direction = new Vector2(Mathf.Sin(sineAngle), Mathf.Cos(sineAngle));
+            angularVel = direction * speed;
+        }
+        else if (modifier == MoveModifier.Loop)
+        {
+            count += Time.deltaTime * modifierIntensity;
+            var sineAngle = angle + Mathf.Sin(count);
+            var direction = new Vector2(Mathf.Sin(sineAngle), Mathf.Cos(sineAngle));
+            angularVel = direction * speed;
+        }
+        else if (modifier == MoveModifier.ZigZag)
+        {
+            count += Time.deltaTime * modifierIntensity;
+            var zzAngle = angle + System.Convert.ToInt32(Mathf.PingPong(count, 1));
+            var direction = new Vector2(Mathf.Sin(zzAngle), Mathf.Cos(zzAngle));
+            angularVel = direction * speed;
+        }
 	}
 
     public ParticleSystem.Particle GetParticle()
